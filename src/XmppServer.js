29cefe58d9e77748da9de6e9196b3245f0293871
SkaveRat <skaverat@funlynx.de>
2016-01-09 15:44:51 +0100

    reformatting XmppServer.js

--- src/XmppServer.js
+++ src/XmppServer.js
@@ -1,87 +1,88 @@
 'use strict';
 
 var xmpp = require('node-xmpp-server')
-    , debug = require('debug')('mero:XmppServer')
-    , _ = require('util')
-    , ltx = require('node-xmpp-core').ltx
-    , EventEmitter = require('events').EventEmitter
-    ;
+  , debug = require('debug')('mero:XmppServer')
+  , _ = require('util')
+  , ltx = require('node-xmpp-core').ltx
+  , EventEmitter = require('events').EventEmitter
+  ;
 
 
 class XmppServer extends EventEmitter {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.stanzas = {
-            presence: "<presence from='%s' to='%s' type='%s'/>"
+    this.stanzas = {
+      presence: "<presence from='%s' to='%s' type='%s'/>"
+    }
+
+    this.xmppServer = new xmpp.Router(5269, '0.0.0.0');
+    this.xmppServer.register("sipgoat.de", (stanza) => this.handleStanza(stanza));
+  }
+
+
+  handleStanza(stanza) {
+    switch (stanza.getName()) {
+      case "presence":
+        debug(stanza.toString());
+        switch (stanza.attrs.type) {
+          case "subscribe":
+            this.emit('xmpp.presence.subscribe', stanza.attrs.from, stanza.attrs.to);
+            break;
+          case "probe":
+            this.xmppServer.send(ltx.parse(_.format(this.stanzas.presence, stanza.attrs.to, stanza.attrs.from, '')));
+            break;
+          default:
+            const status = stanza.getChild('show');
+            if (stanza.attrs.type == 'unavailable') {
+              this.emit('xmpp.presence.status', stanza.attrs.from, 'offline');
+            } else if (status) {
+              this.emit('xmpp.presence.status', stanza.attrs.from, status.getText());
+            } else {
+              this.emit('xmpp.presence.status', stanza.attrs.from, 'online');
+            }
+            break;
         }
 
-        this.xmppServer = new xmpp.Router(5269, '0.0.0.0');
-        this.xmppServer.register("sipgoat.de", (stanza) => this.handleStanza(stanza));
+        break;
+
+      case "message":
+        stanza.children.forEach((element) => {
+          if (element.is("body")) {
+            this.emit('xmpp.message', stanza.attrs['from'], stanza.attrs['to'], element.getText().trim());
+          } else {
+            debug(element);
+          }
+
+        });
+        //debug(util.format("Received message: %s", messageBody.trim()));
+        break;
+      default:
+        debug("Unknown stanza:");
+        debug(stanza.toString());
     }
+  }
 
-    handleStanza(stanza) {
-        switch (stanza.getName()) {
-            case "presence":
-                debug(stanza.toString());
-                switch (stanza.attrs.type) {
-                    case "subscribe":
-                        this.emit('xmpp.presence.subscribe', stanza.attrs.from, stanza.attrs.to);
-                        break;
-                    case "probe":
-                        this.xmppServer.send(ltx.parse(_.format(this.stanzas.presence, stanza.attrs.to, stanza.attrs.from,'')));
-                        break;
-                    default:
-                        const status = stanza.getChild('show');
-                        if(stanza.attrs.type == 'unavailable') {
-                          this.emit('xmpp.presence.status', stanza.attrs.from, 'offline');
-                        }else if (status) {
-                            this.emit('xmpp.presence.status', stanza.attrs.from, status.getText());
-                        }else{
-                            this.emit('xmpp.presence.status', stanza.attrs.from, 'online');
-                        }
-                        break;
-                }
-
-                break;
-
-            case "message":
-                stanza.children.forEach((element) => {
-                    if (element.is("body")) {
-                        this.emit('xmpp.message', stanza.attrs['from'], stanza.attrs['to'], element.getText().trim());
-                    } else {
-                        debug(element);
-                    }
-
-                });
-                //debug(util.format("Received message: %s", messageBody.trim()));
-                break;
-            default:
-                debug("Unknown stanza:");
-                debug(stanza.toString());
-        }
-    }
-
-    sendMessage(from, to, message) {
-        let stanzaTemplate = "<message from='%s' to='%s' type='chat' id='%s'><body>%s</body></message>";
+  sendMessage(from, to, message) {
+    let stanzaTemplate = "<message from='%s' to='%s' type='chat' id='%s'><body>%s</body></message>";
 
 
-        let hrTime = process.hrtime();
-        let stanza = _.format(stanzaTemplate, from, to, (hrTime[0] * 1000000 + hrTime[1]), message);
-        debug(stanza);
-        this.xmppServer.send(ltx.parse(stanza));
-    }
+    let hrTime = process.hrtime();
+    let stanza = _.format(stanzaTemplate, from, to, (hrTime[0] * 1000000 + hrTime[1]), message);
+    debug(stanza);
+    this.xmppServer.send(ltx.parse(stanza));
+  }
 
-    /**
-     * @param from The requester
-     * @param to the matrix user
-     */
-    acceptSubscription(from, to) {
-        var resFormatted = _.format(this.stanzas.presence, to, from, "subscribed");
-        var reqFormatted = _.format(this.stanzas.presence, to, from, "subscribe");
-        this.xmppServer.send(ltx.parse(resFormatted));
-        this.xmppServer.send(ltx.parse(reqFormatted));
-    }
+  /**
+   * @param from The requester
+   * @param to the matrix user
+   */
+  acceptSubscription(from, to) {
+    var resFormatted = _.format(this.stanzas.presence, to, from, "subscribed");
+    var reqFormatted = _.format(this.stanzas.presence, to, from, "subscribe");
+    this.xmppServer.send(ltx.parse(resFormatted));
+    this.xmppServer.send(ltx.parse(reqFormatted));
+  }
 
 }
 
